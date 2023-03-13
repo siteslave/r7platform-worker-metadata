d9d72d0b9fc323e1230faaccc7577e129c4b0247
Satit Rianpit <siteslave@users.noreply.github.com>
2023-03-13 09:16:49 +0700

    update: rewrite promise function for model

--- src/models/metadata.ts
+++ src/models/metadata.ts
@@ -5,38 +5,30 @@ export class MetaModel {
   async savePerson(data: any): Promise<any> {
     const db = await getConnection();
     return new Promise((resolve: any, reject: any) => {
-      try {
-        db('person')
-          .insert(data)
-          .onConflict(['hospcode', 'hn', 'ingress_zone'])
-          .merge(['cid', 'fname', 'lname', 'birth', 'sex', 'd_update', 'updated_at'])
-          .then(() => resolve())
-          .finally(async () => {
-            await db.destroy();
-            resolve();
-          });
-      } catch (error) {
-        reject(error);
-      }
+      db('person')
+        .insert(data)
+        .onConflict(['hospcode', 'hn', 'ingress_zone'])
+        .merge(['cid', 'fname', 'lname', 'birth', 'sex', 'd_update', 'updated_at'])
+        .then(() => resolve())
+        .finally(async () => {
+          await db.destroy();
+          resolve();
+        });
     });
   }
 
   async saveOpd(data: any): Promise<any> {
     const db = await getConnection();
     return new Promise((resolve: any, reject: any) => {
-      try {
-        db('opd')
-          .insert(data)
-          .onConflict(['hospcode', 'hn', 'seq', 'date_serv', 'ingress_zone'])
-          .merge(['time_serv', 'diag_text', 'updated_at'])
-          .then(() => resolve())
-          .finally(async () => {
-            await db.destroy();
-            resolve();
-          });
-      } catch (error) {
-        reject(error);
-      }
+      db('opd')
+        .insert(data)
+        .onConflict(['hospcode', 'hn', 'seq', 'date_serv', 'ingress_zone'])
+        .merge(['time_serv', 'diag_text', 'updated_at'])
+        .then(() => resolve())
+        .finally(async () => {
+          await db.destroy();
+          resolve();
+        });
     });
   }
 
